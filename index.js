5cd5730be91985d013c7d8f6c178d400fcb90641
Verah Mokaya <verahmokaya475@gmail.com>
2025-09-01 20:08:59 +0600

    arithematic lab completed

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+// max number should be 20
+const max = Math.max(1,3,6,13,18,20);
+
+// a number divided by a number is equal to 4
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+// a random number above 0
+const random = Math.floor(Math.random() * 10 + 1);
+
+const num1 = 31;
+const num2 = 2;
+const multiply = num2 * num1;
